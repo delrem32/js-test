b51edb51316700464ff0c33212f0fb3be9602f10
delrem32 <delrem32@gmail.com>
2020-08-18 21:39:42 +0200

    wtf

--- index.js
+++ index.js
@@ -2,6 +2,23 @@
 import './style.css';
 
 // Write Javascript code!
+'use strict';
+
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let me = "fuck";
+if(true) {
+  let ttt = () => {
+    return console.log(me);
+};
+}
+ttt();
+
+
+
+
+
+
+
 
