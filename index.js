6ce87c2e9e49f582742824983a2ab3f845ce24a4
delrem32 <delrem32@gmail.com>
2020-08-26 19:49:30 +0200

    1 file modified

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-let me = "fuck";
-if(true) {
-  let ttt = () => {
-    return console.log(me);
-};
-}
-ttt();
 
 
 
