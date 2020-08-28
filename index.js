a0f5fbc36859a714f80dc807646929848f1e0c1f
delrem32 <delrem32@gmail.com>
2020-08-28 22:30:27 +0200

    1 file modified

--- index.js
+++ index.js
@@ -7,26 +7,18 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const data = {
-  'Andrew': {name: 'Andrew', age: 12},
-  'Baer':{name: 'Baer', age: 14},
-  'Cerr':{name: 'Cerr', age: 15},
-  'Deadr':{name: 'Deadr', age: 16}
-};
+var a = 15;
 
-const b = Object.values(data);
+function bla() {
+   var a;
+   console.log(a);
+   a = 25;
+   function result() {
+      return console.log(a);
+   }
+   result.a = 20;
+   return result();
+}
 
-console.log(data)
-
-
-const data2 = [
-  {name: 'Andrew', age: 12},
-  {name: 'Baer', age: 14},
-  {name: 'Cerr', age: 15},
-  {name: 'Deadr', age: 16}
-];
-const exp = ['Cerr', 'Baer'];
-const c = exp.map(name => data2.filter(obj => obj.name === name)).flat();
-const t = data2.filter(i => exp.includes(i.name));
-console.log(c);
-console.log(t);
+var foo = bla.bind({a: 45});
+foo();
